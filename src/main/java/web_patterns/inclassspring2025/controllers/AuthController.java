a2e30be81b78d1714ab180e1d42cd1d099ddd851
michelle <michelle.graham@dkit.ie>
2025-12-09 11:06:40 +0000

    fix: amend skeleton for controller-type classes to include overall request mapping for all endpoints under this controller (AuthController here)

--- src/main/java/web_patterns/inclassspring2025/controllers/AuthController.java
+++ src/main/java/web_patterns/inclassspring2025/controllers/AuthController.java
@@ -1,15 +1,19 @@
 package web_patterns.inclassspring2025.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import web_patterns.inclassspring2025.services.AuthService;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/auth/")
 public class AuthController {
     private AuthService authService;
 
     public AuthController(AuthService authService){
         this.authService = authService;
     }
+
+
 }
